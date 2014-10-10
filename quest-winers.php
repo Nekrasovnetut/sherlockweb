ab39f9a252762ec47bc90e7d343be8b3d5280cc5
Nekrasovnetut <nekrasov@netut.ru>
2014-10-10 21:02:36 +0400

    Начал работать со страницей победителей

---
+++ quest-winers.php
@@ -0,0 +1,16 @@
+<? 	$pageTitle = "Главная 2";
+	
+	include "includes/header.php" ?>
+
+
+<div class="page">
+	<div class="section">
+		<header class="section-heading">
+			<h2>Лучшая команда</h2>
+			<h3>В квесте диплодоки в ловушке</h3>
+		</header>
+	</div>
+</div>
+
+<? include "includes/scripts.php" ?>
+<? include "includes/footer.php" ?>
