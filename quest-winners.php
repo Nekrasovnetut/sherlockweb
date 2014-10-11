17d24e77e2f5830c2ac955a029b26ebe4809ce36
John Grishin <r@exah.ru>
2014-10-11 14:21:24 +0400

    Добавил page-heading и внёс другие правки
    Signed-off-by: John Grishin <r@exah.ru>

--- quest-winners.php
+++ quest-winners.php
@@ -1,14 +1,16 @@
 <? 	$pageTitle = "Главная 2";
+	$pageClass = "collection-1";
 	
 	include "includes/header.php" ?>
 
 
 <div class="page">
+	<header class="page-heading">
+		<h2>Лучшая команда</h2>
+		<p>В квесте &laquo;Диплодоки в ловушке&raquo;</p>
+	</header>
 	<div class="section">
-		<header class="section-heading">
-			<h2>Лучшая команда</h2>
-			<h3>В квесте диплодоки в ловушке</h3>
-		</header>
+		
 	</div>
 </div>
 
