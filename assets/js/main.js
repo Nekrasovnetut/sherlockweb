c3a03dfcf53c0b037987d2d4adcfc23114af346f
John Grishin <r@exah.ru>
2014-10-14 13:12:14 +0400

    Вкладки

--- assets/js/main.js
+++ assets/js/main.js
@@ -63,4 +63,43 @@
 		}
 	});
 	
+	$('.tabs').on('click', '.tabs-item a', function(e){
+		var $t = $(this),
+			$tab = $(this).parent('.tabs-item');
+			
+		e.preventDefault();
+		
+		if (!$tab.hasClass('is-active')) {
+			$(this).removeClass('is-active');
+			$('.tabs-item.is-active').removeClass('is-active');
+			
+			
+			$('.tabs-content-item.is-active').velocity({
+				opacity: 0,
+				translateY: 50
+			}, {
+				duration: 400,
+				display: 'none',
+				begin: function(){
+					$('.tabs-content').height($($t[0].hash).innerHeight())
+				},
+				complete: function(){
+					$tab.addClass('is-active');
+					
+					$($t[0].hash).velocity({translateY: -30}, 0).velocity({
+						opacity: 1,
+						translateY: 0
+					}, {
+						duration: 400,
+						display: 'block',
+						complete: function(){
+							$(this).addClass('is-active');
+						}
+					});
+				}
+			});
+			
+		}
+	});
+	
 }(jQuery));
