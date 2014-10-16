8caed5ca1e060c32b13d16597e9ed958ce19ac68
Nekrasovnetut <nekrasov@netut.ru>
2014-10-16 22:44:27 +0400

    И еще файл

---
+++ quest-winners.php
@@ -0,0 +1,111 @@
+<? 	$pageTitle = "Лучшая команда в квесте";
+	$pageClass = "page-collection-1";
+	$collection1color = '#26C0D5';
+	
+	include "includes/header.php" ?>
+
+
+<div class="page <?= $pageClass ?>">
+	<header class="page-heading">
+		<h3>Лучшая команда</h3>
+		<p>В квесте &laquo;Диплодоки в ловушке&raquo;</p>
+	</header>
+	<div class="winners">
+		<section class="section">	
+			<table>
+				
+				<tr class="winners-header">
+					<td>
+						Место
+					</td>
+					
+					<td>
+						Команда
+					</td>
+					
+					<td>
+						Время
+					</td>
+					
+					<td>
+						Дата
+					</td>
+					
+					<td>
+						Фото
+					</td>
+				</tr>
+				
+				<tr class="winners-userteam">
+					<td>
+						1
+					</td>
+					
+					<td>
+						Александр Убойный, Иван Петров,  Рулон Обоев 
+					</td>
+					
+					<td>
+						23:45
+					</td>
+					
+					<td>
+						21.09.2012
+					</td>
+					
+					<td>
+						Фото
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						2
+					</td>
+					
+					<td>
+						Иван Петров,  Рулон Обоев, Александр Убойный
+					</td>
+					
+					<td>
+						27:55
+					</td>
+					
+					<td>
+						22.09.2012
+					</td>
+					
+					<td>
+						Фото
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						3
+					</td>
+					
+					<td>
+						Петр Петров,  Рулон Обоев, Александр Убойный, 
+					</td>
+					
+					<td>
+						34:15
+					</td>
+					
+					<td>
+						22.09.2012
+					</td>
+					
+					<td>
+						Фото
+					</td>
+				</tr>
+				
+			</table>	
+		</section>
+	</div>
+</div>
+
+<? include "includes/scripts.php" ?>
+<? include "includes/footer.php" ?>
