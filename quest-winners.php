1c8c4b01098ee17dca5a83050f75db98dd5624a3
Nekrasovnetut <nekrasov@netut.ru>
2014-10-17 19:09:57 +0400

    Победители

--- quest-winners.php
+++ quest-winners.php
@@ -6,43 +6,52 @@
 
 
 <div class="page <?= $pageClass ?>">
+	<header class="page-cover">
+	<div class="page-cover-bg">
+		<img src="example-images/example-cover.jpg" alt="" />
+	</div>
+	<div class="page-cover-content">
+	
+	</div>
+	</header>
 	<header class="page-heading">
 		<h3>Лучшая команда</h3>
 		<p>В квесте &laquo;Диплодоки в ловушке&raquo;</p>
 	</header>
+	<div><a href="quest.php">Квест</a></div>
 	<div class="winners">
 		<section class="section">	
-			<table>
+			<table class="winners-table">
 				
-				<tr class="winners-header">
-					<td>
+				<tr class="winners-table-header">
+					<td class="winners-table-mesto">
 						Место
 					</td>
 					
-					<td>
+					<td class="winners-table-comanda">
 						Команда
 					</td>
 					
-					<td>
+					<td class="winners-table-vremya">
 						Время
 					</td>
 					
-					<td>
+					<td class="winners-table-data">
 						Дата
 					</td>
 					
-					<td>
+					<td class="winners-table-photo">
 						Фото
 					</td>
 				</tr>
 				
-				<tr class="winners-userteam">
+				<tr class="winners-table-userteam">
 					<td>
-						1
+						256
 					</td>
 					
 					<td>
-						Александр Убойный, Иван Петров,  Рулон Обоев 
+						Убойный, Петров, Обоев 
 					</td>
 					
 					<td>
@@ -58,13 +67,13 @@
 					</td>
 				</tr>
 				
-				<tr>
+				<tr class="winners-table-team">
 					<td>
 						2
 					</td>
 					
 					<td>
-						Иван Петров,  Рулон Обоев, Александр Убойный
+						Петров, Обоев, Убойный
 					</td>
 					
 					<td>
@@ -80,13 +89,57 @@
 					</td>
 				</tr>
 				
-				<tr>
+				<tr class="winners-table-team">
 					<td>
 						3
 					</td>
 					
 					<td>
-						Петр Петров,  Рулон Обоев, Александр Убойный, 
+						Петров, Обоев, Убойный, 
+					</td>
+					
+					<td>
+						34:15
+					</td>
+					
+					<td>
+						22.09.2012
+					</td>
+					
+					<td>
+						Фото
+					</td>
+				</tr>
+				
+				<tr class="winners-table-team">
+					<td>
+						2
+					</td>
+					
+					<td>
+						Петров, Обоев, Убойный
+					</td>
+					
+					<td>
+						27:55
+					</td>
+					
+					<td>
+						22.09.2012
+					</td>
+					
+					<td>
+						Фото
+					</td>
+				</tr>
+				
+				<tr class="winners-table-team">
+					<td>
+						3
+					</td>
+					
+					<td>
+						Петров, Обоев, Убойный, 
 					</td>
 					
 					<td>
