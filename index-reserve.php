ee0b5f456aff265f33e0e04a0855c8f6415f7665
John Grishin <r@exah.ru>
2014-10-18 18:01:26 +0400

    Навигация по дням в резерве на главной

--- index-reserve.php
+++ index-reserve.php
@@ -34,26 +34,43 @@
 			<li class="nav-item"><a href="index-map.php">Карта</a></li>
 		</ul>
 	</nav>
-	<section class="section collection-schedule" style="background-color: #26C0D5;">
-		<header class="section-heading"><a href="collection-list.php">
-			<h3> Путешествия </h3>
-		</a></header>
-		<div class="collection-content aligner">
-			<div class="schedule-overflow">
-				<? include "includes/misc/schedule-reserve.php" ?>
+	<div class="collection-0"> <!-- @todo <- fix this -->
+	<nav class="tabs nav-days">
+		<ul>
+			<li class="nav-item nav-item-heading"><span>12</span> Cентября</li>
+
+			<? for ($i = 1; $i <= 30; $i++) { ?>
+				<li class="tabs-item nav-item <?= ($i == 1) ? "is-active" : "" ?> "><a href="#day-<?= $i ?>"> <?= $i ?> </a></li>
+			<? } ?>
+		</ul>
+	</nav></div>
+	<div class="tabs-content" style="background-color: #26C0D5;">
+
+		<? for ($i = 1; $i <= 30; $i++) { ?>
+			<div id="day-<?= $i ?>" class="tabs-content-item <?= ($i == 1) ? "is-active" : "" ?> ">
+				<section class="section collection-schedule" style="background-color: #26C0D5;">
+					<header class="section-heading"><a href="collection-list.php">
+						<h3> Путешествия </h3>
+					</a></header>
+					<div class="collection-content aligner">
+						<div class="schedule-overflow">
+							<? include "includes/misc/schedule-reserve.php" ?>
+						</div>
+					</div>
+				</section>
+				<section class="section collection-schedule" style="background-color: #89CC32;">
+					<header class="section-heading"><a href="collection-list.php">
+						<h3> Сериалы </h3>
+					</a></header>
+					<div class="collection-content aligner">
+						<div class="schedule-overflow">
+							<? include "includes/misc/schedule-reserve.php" ?>
+						</div>
+					</div>
+				</section>
 			</div>
-		</div>
-	</section>
-	<section class="section collection-schedule" style="background-color: #89CC32;">
-		<header class="section-heading"><a href="collection-list.php">
-			<h3> Сериалы </h3>
-		</a></header>
-		<div class="collection-content aligner">
-			<div class="schedule-overflow">
-				<? include "includes/misc/schedule-reserve.php" ?>
-			</div>
-		</div>
-	</section>
+		<? } ?>
+	</div>
 </div>
 
 <? include "includes/scripts.php" ?>
