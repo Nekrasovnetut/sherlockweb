427080161bfe9a8a9293ebe55fa3ebcc901e66f4
John Grishin <r@exah.ru>
2014-10-18 16:20:01 +0400

    Fixed stashed changes

--- quest-winners.php
+++ quest-winners.php
@@ -11,7 +11,7 @@
 		<img src="example-images/example-cover.jpg" alt="" />
 	</div>
 	<div class="page-cover-content">
-	
+
 	</div>
 	</header>
 	<header class="page-heading">
@@ -20,34 +20,32 @@
 	</header>
 	<div><a href="quest.php">Квест</a></div>
 	<div class="winners">
-<<<<<<< Updated upstream
-		<section class="section">	
+		<section class="section">
 			<table class="winners-table">
-				
+
 				<tr class="winners-table-header">
 					<td class="winners-table-mesto">
 						Место
 					</td>
-					
+
 					<td class="winners-table-comanda">
 						Команда
 					</td>
-					
+
 					<td class="winners-table-vremya">
 						Время
 					</td>
-					
+
 					<td class="winners-table-data">
 						Дата
 					</td>
-					
+
 					<td class="winners-table-photo">
 						Фото
 					</td>
 				</tr>
-				
+
 				<tr class="winners-table-userteam">
-=======
 		<section class="section">
 			<table>
 
@@ -74,17 +72,13 @@
 				</tr>
 
 				<tr class="winners-userteam">
->>>>>>> Stashed changes
 					<td>
 						256
 					</td>
 
 					<td>
-<<<<<<< Updated upstream
-						Убойный, Петров, Обоев 
-=======
+						Убойный, Петров, Обоев
 						Александр Убойный, Иван Петров,  Рулон Обоев
->>>>>>> Stashed changes
 					</td>
 
 					<td>
@@ -99,57 +93,54 @@
 						Фото
 					</td>
 				</tr>
-<<<<<<< Updated upstream
-				
+
 				<tr class="winners-table-team">
 					<td>
 						2
 					</td>
-					
+
 					<td>
 						Петров, Обоев, Убойный
 					</td>
-					
+
 					<td>
 						27:55
 					</td>
-					
+
 					<td>
 						22.09.2012
 					</td>
-					
+
 					<td>
 						Фото
 					</td>
 				</tr>
-				
+
 				<tr class="winners-table-team">
 					<td>
 						3
 					</td>
-					
+
 					<td>
-						Петров, Обоев, Убойный, 
+						Петров, Обоев, Убойный,
 					</td>
-					
+
 					<td>
 						34:15
 					</td>
-					
+
 					<td>
 						22.09.2012
 					</td>
-					
+
 					<td>
 						Фото
 					</td>
 				</tr>
-				
+
 				<tr class="winners-table-team">
-=======
 
 				<tr>
->>>>>>> Stashed changes
 					<td>
 						2
 					</td>
@@ -170,23 +161,17 @@
 						Фото
 					</td>
 				</tr>
-<<<<<<< Updated upstream
-				
+
 				<tr class="winners-table-team">
-=======
 
 				<tr>
->>>>>>> Stashed changes
 					<td>
 						3
 					</td>
 
 					<td>
-<<<<<<< Updated upstream
-						Петров, Обоев, Убойный, 
-=======
+						Петров, Обоев, Убойный,
 						Петр Петров,  Рулон Обоев, Александр Убойный,
->>>>>>> Stashed changes
 					</td>
 
 					<td>
