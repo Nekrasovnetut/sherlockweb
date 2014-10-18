86bc0154081691e47344a3f8b5a3506b70cb828c
Nekrasovnetut <nekrasov@netut.ru>
2014-10-18 16:21:34 +0400

    Page Collection-list
    New Collection-list page, include of preview info for quest and some
    dirty staff

---
+++ collection-list.php
@@ -0,0 +1,57 @@
+<? 	$pageTitle = "Путешествия";
+	
+	include "includes/header.php" ?>
+
+<header class="page-cover">
+	<div class="page-cover-bg">
+		<img src="example-images/example-cover.jpg" alt="" />
+	</div>
+	<div class="page-cover-content">
+	
+	</div>
+</header>
+
+<div class="page collection-list">
+	<section class="section">
+		<div class="collection-list-item">
+			<div class="collection-list-item-img to-left">
+				<figure class="covermini">
+					<span class="covermini-age">+18</span>
+					<img src="example-images/example-cover-mini.png">
+				</figure>
+			</div>
+			<div class="collection-list-item-content to-right">
+				
+				<div class="collection-list-item-content-top mcf">
+					<h3 class="to-left">Текила бум</h3>
+					<div class="collection-list-item-content-top-info to-right"><? include "includes/quest-preview.php" ?></div>
+				</div>
+				
+				<hr />
+				
+				<div class="collection-list-item-content-middle">
+					<p>
+					Судья Люк Фонтен принял решение на основании позиции прокуратуры Лиона, которая ранее дала согласие на выдачу Кузнецова. Возражения адвокатов бывшего министра, настаивавших на политических мотивах преследования их клиента, были отвергнуты. Вместе с тем решение суда сопровождается рядом оговорок. Как сообщили судебные источники, Кузнецова нельзя будет привлечь в качестве наказания к принудительному труду. Кроме того, ему должны позволить поддерживать регулярные контакты с французскими чиновниками.
+					</p>
+				</div>
+				
+				<div class="collection-list-item-content-bottom">
+					<div class="collection-list-item-content-bottom-adress to-left">
+						<i class="icon-user"></i>
+						М. Кропоткинская Большой Афонасьевский пер 15 стр1
+					</div>
+					<a href="btn" class="btn to-right">Записаться</a>
+				</div>
+			</div>
+		
+		</div>
+	</section>
+	
+
+	
+</div>
+
+
+
+<? include "includes/scripts.php" ?>
+<? include "includes/footer.php" ?>
---
+++ includes/quest-preview.php
@@ -0,0 +1,14 @@
+			<dl class="quest-info">
+				<dt class="quest-info-label">
+					<i class="icon-user"></i> 
+					<span>Количество <br /> человек</span>
+				</dt>
+				<dd class="quest-info-content">2-4 человек</dd>
+			</dl>
+			<dl class="quest-info">
+				<dt class="quest-info-label">
+					<i class="icon-clock"></i> 
+					<span>Время <br /> прохождения</span>
+				</dt>
+				<dd class="quest-info-content">60 минут</dd>
+			</dl>
