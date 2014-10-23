eecf5ff5255fb43f4e196d2fd54e1a9128a44349
Nekrasovnetut <nekrasov@netut.ru>
2014-10-23 14:15:33 +0400

    Начал делать букинг (бронирование)

---
+++ booking-final.php
@@ -0,0 +1,27 @@
+<? 	$pageTitle = "Главная";
+	$pageClass = "page-collection-map-1";
+	$collection1color = '#26C0D5';
+
+	include "includes/header.php" ?>
+
+
+
+<div class="page textpage">
+
+<section class="section">
+	
+	<h2>Квест забронирован</h2>
+	
+	<p>
+		Подробную информация о нём вы можете
+посмотреть его вы можете в своем личном кабинете
+	</p>
+		
+	<a href="#" class="btn">Распечатать</a>
+	
+</section>
+
+</div>
+
+<? include "includes/scripts.php" ?>
+<? include "includes/footer.php" ?>
