0d4917a1ca67084cd33a832038d2aa015cc9b5b6
Nekrasovnetut <nekrasov@netut.ru>
2014-10-26 14:58:56 +0300

    New page

---
+++ booking-pay.php
@@ -0,0 +1,79 @@
+<? 	$pageTitle = "Квест забронирован";
+	$pageClass = "page-collection-map-1";
+	$collection1color = '#26C0D5';
+
+	include "includes/header.php" ?>
+
+
+
+<div class="page booking-pay">
+		
+	<section class="section">
+		
+		<div class="quest-item booking-final-item">
+			<div class="quest-item-img">
+				<figure class="covermini">
+					<div class="age">+18</div>
+					<img src="example-images/example-cover-mini.png">
+				</figure>
+			</div>
+			<div class="quest-item-content">
+
+				<div class="quest-item-top">
+					<h3 class="quest-item-top-left booking-final-item-bigname">Сибирь: Старая лаборатория</h3>
+					<div class="quest-item-top-right">
+						<div class="booking-final-item-day">21 Сентября 14:45</div>
+						<div class="booking-final-item-price"> 2000 руб</div>
+					</div>
+				</div>
+
+				<hr />
+
+		
+
+				<div class="quest-item-bottom booking-final-item-adress">
+					<div class="quest-item-bottom-left">
+						<div class="quest-item-bottom-icon">
+						<i class="icon-marker"></i>
+						</div>
+						<div class="quest-item-bottom-adress">			
+						<ul>
+							<li>М. Кропоткинская</li>
+							<li>Большой Афонасьевский пер 15 стр1 </li>
+						</ul>
+						</div>
+					</div>
+				
+				</div>
+			</div>
+
+		</div>
+				
+	</section>
+	
+	<section class="section">
+		
+		<div class="booking-pay-variant booking-pay-booked">
+		<h4>Забронировать</h4>
+		<p>Вы можете забронировать игру и оплатить ее на месте перед прохождением деньгами или купоном</p>
+		<a href="#" class="btn btn-outline">Бронировать</a>
+	</div>
+		
+		<div class="booking-pay-variant booking-pay-cupon">
+		<h4>Использовать подарочный купон</h4>
+		<p>Используйте свой подарочный купон для оплаты квеста</p>
+		<a href="#" class="btn btn-outline">Использовать купон</a>
+	</div>
+		
+		<div class=" booking-pay-variant booking-pay-booked">
+		<h4>Забронировать и оплатить</h4>
+		<p>Вы можете забронировать игру и оплатить ее прямо сейчас</p>
+		<a href="#" class="btn btn-outline">Оплатить</a>
+	</div>
+		
+	</section>
+
+</div>
+
+<? include "includes/scripts.php" ?>
+<? include "includes/footer.php" ?>
