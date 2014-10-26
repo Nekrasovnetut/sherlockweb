bfccbd1e251ffb716cfc4bcc60dbb45b49e7ea11
John Grishin <r@exah.ru>
2014-10-26 18:09:12 +0300

    iconInfo fixes

--- includes/quest-preview.php
+++ includes/quest-preview.php
@@ -1,27 +1,29 @@
-			<dl class="quest-info">
-				<dt class="quest-info-heading">
-					<dl class="iconInfo">
-						<dt class="iconInfo-icon">
-							<i class="icon-user"></i>
-						</dt>
-						<dt class="iconInfo-content">
-							<span class="accent">Время прохождения</span>
-						</dt>
-					</dl>
-				</dt>
+			<div class="quest-info--container">
+				<dl class="quest-info">
+					<dt class="quest-info-heading">
+						<dl class="iconInfo">
+							<dt class="iconInfo-icon">
+								<i class="icon-user"></i>
+							</dt>
+							<dt class="iconInfo-content">
+								<span class="accent">Время прохождения</span>
+							</dt>
+						</dl>
+					</dt>
 
-				<dd class="quest-info-content">2-4 человек</dd>
-			</dl>
-			<dl class="quest-info">
-				<dt class="quest-info-heading">
-					<dl class="iconInfo">
-						<dt class="iconInfo-icon">
-							<i class="icon-clock"></i>
-						</dt>
-						<dt class="iconInfo-content">
-							<span class="accent">Время прохождения</span>
-						</dt>
-					</dl>
-				</dt>
-				<dd class="quest-info-content">60 минут</dd>
-			</dl>
+					<dd class="quest-info-content">2-4 человек</dd>
+				</dl>
+				<dl class="quest-info">
+					<dt class="quest-info-heading">
+						<dl class="iconInfo">
+							<dt class="iconInfo-icon">
+								<i class="icon-clock"></i>
+							</dt>
+							<dt class="iconInfo-content">
+								<span class="accent">Время прохождения</span>
+							</dt>
+						</dl>
+					</dt>
+					<dd class="quest-info-content">60 минут</dd>
+				</dl>
+			</div>
