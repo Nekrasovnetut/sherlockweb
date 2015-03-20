be55baefe772aad7406e95671b61ca64b96d3246
Creat1ve <e6awy.v.kawy@gmail.com>
2015-03-20 03:31:30 +0200

    Новая верстка (Mobile and Desktop)

---
+++ feedback.php
@@ -0,0 +1,195 @@
+<?
+	$pageTitle = "Отзывы";
+	include "includes/header.php";
+?>
+<div class="fixed-top">
+	<header class="page-cover">
+	<div class="page-cover-bg">
+		<img src="example-images/cover-feedback.jpg" alt="" />
+	</div>
+	<div class="page-cover-content">
+	
+	</div>
+	</header>
+</div>
+<div class="js-ghost"></div>
+
+<div class="content page">
+	<ul class="middle-menu">
+		<li><span class="players active"></span><a id="players" class="tabs-link active">Игроки</a></li>
+		<li><span class="media"></span><a id="media" class="tabs-link">Медиа</a></li>
+	</ul>
+	<section class="section">
+		<div class="tab-players">
+			<div class="player-block">
+				<div class="avatar">
+					<img src="/assets/img/feedback_certificate/avatars/avatar_1.jpg" alt="" />
+				</div>
+				<div class="player">
+					<div class="player-name">Архип <span class="br"></span>Семёнов</div>
+					<div class="player-fb">
+						<a href="#"></a>
+					</div>
+					<div class="player-special">Игрок по жизни</div>
+					<div class="player-text">
+						<p>
+							Это вообще лучшее что случалось со мной, со времён Роки и Бульвинкля, картошки в мундире и теории петлевой квантовой гравитации. Жжете ребята!
+						</p>
+					</div>
+					<div class="player-info">
+						Игры: <span class="game">Лаборатория</span>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			
+			<div class="player-block">
+				<div class="avatar">
+					<img src="/assets/img/feedback_certificate/avatars/avatar_2.jpg" alt="" />
+				</div>
+				<div class="player">
+					<div class="player-name">Юлия <span class="br"></span>Мельникова</div>
+					<div class="player-fb">
+						<a href="#"></a>
+					</div>
+					<div class="player-special">Художественный руководитель Парка Горького</div>
+					<div class="player-text">
+						<p>
+							В какой-то момент грань между игрой и реальностью стирается. Но в этот миг как раз и начинается самое интересное…
+						</p>
+					</div>
+					<div class="player-info">
+						Игры: <span class="game">Упавшее небо</span>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			
+			<div class="player-block">
+				<div class="avatar">
+					<img src="/assets/img/feedback_certificate/avatars/avatar_3.jpg" alt="" />
+				</div>
+				<div class="player">
+					<div class="player-name">Александр <span class="br"></span>Маленков</div>
+					<div class="player-fb">
+						<a href="#"></a>
+					</div>
+					<div class="player-special">Главный редактор журнала MAXIM</div>
+					<div class="player-text">
+						<p>
+							В какой-то момент грань между игрой и реальностью стирается. Но в этот миг как раз и начинается самое интересное… В какой-то момент грань между игрой и реальностью стирается. Но в этот миг как раз и начинается самое интересное…
+						</p>
+					</div>
+					<div class="player-info">
+						Игры: <span class="game">Мышеловка для гринго</span>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			
+			<div class="player-block">
+				<div class="avatar">
+					<img src="/assets/img/feedback_certificate/avatars/avatar_3.jpg" alt="" />
+				</div>
+				<div class="player">
+					<div class="player-name">Александр <span class="br"></span>Маленков</div>
+					<div class="player-fb">
+						<a href="#"></a>
+					</div>
+					<div class="player-special">Главный редактор журнала MAXIM</div>
+					<div class="player-text">
+						<p>
+							В какой-то момент грань между игрой и реальностью стирается. Но в этот миг как раз и начинается самое интересное… В какой-то момент грань между игрой и реальностью стирается. Но в этот миг как раз и начинается самое интересное…
+						</p>
+					</div>
+					<div class="player-info">
+						Игры: <span class="game">Упавшее небо</span> <span class="game">Мышеловка для гринго</span>
+					</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			
+			<a class="more-comments" href="#">Ещё отзывы</a>
+		</div>
+		<div class="tab-media">
+			<div class="media-block">
+				<div class="avatar">
+					<img src="/assets/img/feedback_certificate/media/media_1.png" alt="" />
+				</div>
+				<div class="context">
+					<div class="media-text">
+						<p>
+							Это вообще лучшее что случалось со мной, со времён Роки и Бульвинкля, картошки в мундире и теории петлевой квантовой гравитации. Жжете ребята!
+						</p>
+					</div>
+					<div class="media-date">11.05.2014</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			
+			<div class="media-block">
+				<div class="avatar">
+					<img src="/assets/img/feedback_certificate/media/media_2.png" alt="" />
+				</div>
+				<div class="context">
+					<div class="media-text">
+						<p>
+							В какой-то момент грань между игрой и реальностью стирается. Но в этот миг как раз и начинается самое интересное…
+						</p>
+					</div>
+					<div class="media-date">9.05.2014</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			
+			<div class="media-block">
+				<div class="avatar">
+					<img src="/assets/img/feedback_certificate/media/media_3.png" alt="" />
+				</div>
+				<div class="context">
+					<div class="media-text">
+						<p>
+							В какой-то момент грань между игрой и реальностью стирается. Но в этот миг как раз и начинается самое интересное… В какой-то момент грань между игрой и реальностью стирается. Но в этот миг как раз и начинается самое интересное… В какой-то момент грань между игрой и реальностью стирается. Но в этот миг как раз и начинается самое интересное… 
+						</p>
+					</div>
+					<div class="media-date">1.05.2014</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			
+			<div class="media-block">
+				<div class="avatar">
+					<img src="/assets/img/feedback_certificate/media/media_4.png" alt="" />
+				</div>
+				<div class="context">
+					<div class="media-text">
+						<p>
+							В какой-то момент грань между игрой и реальностью стирается. Но в этот миг как раз и начинается самое интересное…
+						</p>
+					</div>
+					<div class="media-date">1.05.2014</div>
+				</div>
+				<div class="clear"></div>
+			</div>
+			
+			<a class="more-comments" href="#">Ещё отзывы</a>
+		</div>
+	</section>
+	<div class="bottom-social">
+		<div class="middle">
+			<div class="can-also">
+				Вы так же можете НАписать или прочитать<br>отзыв о нас в соцсетях:
+			</div>
+			<ul class="social-big">
+				<li><a href="#" class="fb"></a></li>
+				<li><a href="#" class="ff"></a></li>
+				<li><a href="#" class="ig"></a></li>
+				<li><a href="#" class="ss"></a></li>
+				<li><a href="#" class="vk"></a></li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<? include "includes/scripts.php" ?>
+<? include "includes/footer.php" ?>
